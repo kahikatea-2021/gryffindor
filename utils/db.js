d40ea25594380ed1282fff6c2a6ae5d55458070d
Dmitriy <dmtiriy.yegorov85@gmail.com>
2021-04-02 14:55:25 +1300

    Minor changes

--- utils/db.js
+++ utils/db.js
@@ -1,39 +1,35 @@
 const config = require('../knexfile').development
 const database = require('knex')(config)
 
-function getAllSpells(db = database) {
+function getAllSpells (db = database) {
   return db('spells').select()
 }
 
-function close(db = database) {
+function close (db = database) {
   db.destroy()
 }
 
-function getRandomId(db = database) {
+function getRandomId (db = database) {
   min = Math.ceil(1)
   max = Math.floor(10)
-  return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min)
 }
 
-function getSpellById(id, db = database) {
+function getSpellById (id, db = database) {
   return db('spells')
     .where('spells.id', id).first()
 }
 
-function addNewSpell(newSpell, db = database) {
+function addNewSpell (newSpell, db = database) {
   return db('spells')
     .insert({
       name: newSpell.name,
       description: newSpell.description,
       img: newSpell.img
     })
-    .then(result => {
-      console.log('result: ', result)
-      return db('spells')
-    })
 }
 
-function deleteSpell(id, db = database) {
+function deleteSpell (id, db = database) {
   return db('spells')
     .where({ id: id }).del()
 }
